b39bfaf8b2663fe129ea52b6a539ca6f798bdf10
Sachin Thokale <sachinthokale999@gmail.com>
2023-09-17 13:29:01 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import { userRouter } from "./routes/User.js";
 import cookieParser from "cookie-parser";
 // import cors from "cors";
 import cors from "cors";
+import path from "path";
 
 export const app = express();
 app.use(cors({ origin: "http://localhost:3000" }));
@@ -11,3 +12,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
 app.use("/api/v1", userRouter);
+app.use(express.static(path.resolve("/frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("./frontend/build/index.html"));
+});
