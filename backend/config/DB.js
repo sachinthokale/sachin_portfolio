0f2c188036173b6ca342157e8cdc817e429ec36e
Sachin Thokale <sachinthokale999@gmail.com>
2023-09-18 12:09:38 +0530

    sixth

--- backend/config/DB.js
+++ backend/config/DB.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = () => {
-    mongoose.connect(process.env.MONGO_URI).then((c) =>
-        console.log(`mongodb connected to ${c.connection.host}`)).catch(
-            e => console.log(e)
-        )
-}
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    .then((c) => console.log(`mongodb connected to ${c.connection.host}`))
+    .catch((e) => console.log(e));
+};
