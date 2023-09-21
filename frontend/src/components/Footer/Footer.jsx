8fbf71f8dd3c4aba0bb3fe03306aa13ba7a093b4
Sachin Thokale <sachinthokale999@gmail.com>
2023-09-21 12:34:25 +0530

    eleven

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import "./Footer.css"
-import { Typography } from '@mui/material'
-import { Link } from "react-router-dom"
-import GitHubIcon from '@mui/icons-material/GitHub';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import React from "react";
+import "./Footer.css";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 const Footer = () => {
-    return (
-        <div className='footer'>
-            <div className="footer-contact-us">
-                <Typography variant='h5'>About Me</Typography>
-                <Typography >Hey, my name is Sachin Thokale. I am a MERN-Stack Developer
-                    and UI/UX Developer...
-                </Typography>
-                <Link to="/contact" className='footer-contact-btn'>
-                    <Typography variant='h9'>Contact Us</Typography>
-                </Link>
-            </div>
-            <div className="social-media">
-                <Typography variant='h6'>Social Media</Typography>
-                <a href="">
-                    <GitHubIcon />
-                </a>
-                <a href="https://www.instagram.com/" target='blank'>
-                    <InstagramIcon />
-                </a>
-                <a href="">
-                    <LinkedInIcon />
-                </a>
-            </div>
+  return (
+    <div className="footer">
+      <div className="footer-contact-us">
+        <Typography variant="h5">About Me</Typography>
+        <Typography>
+          Hey, my name is Sachin Thokale. I am a MERN-Stack Developer and UI/UX
+          Developer...
+        </Typography>
+        <Link to="/contact" className="footer-contact-btn">
+          <Typography variant="h9">Contact Us</Typography>
+        </Link>
+      </div>
+      <div className="social-media">
+        <Typography variant="h6">Social Media</Typography>
+        <a href="https://github.com/sachinthokale?tab=repositories">
+          <GitHubIcon />
+        </a>
+        <a href="https://www.instagram.com/" target="blank">
+          <InstagramIcon />
+        </a>
+        <a href="www.linkedin.com/in/sachin-thokale-39bb1320b">
+          <LinkedInIcon />
+        </a>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Footer
+export default Footer;
